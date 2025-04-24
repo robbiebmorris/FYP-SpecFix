c58e9f7e4c2566f96e34cd96ce6efe8b5facad76
Robbie Morris <robbiebmorris@gmail.com>
2025-04-24 21:49:13 +0100

    bug fix

--- experiment/example_interpretation/experiment.py
+++ experiment/example_interpretation/experiment.py
@@ -29,7 +29,7 @@ def parse_problem(problem):
     return requirement, repaired_requirement, entry_point, examples, task_id
 
 
-def process_problem(i, problem, inputs, outputs, evaluator, n_programs, model_name, n_shot):
+def process_problem(i, problem, inputs, outputs, evaluator, n_programs, model_name):
     requirement, repaired_requirement, entry_point, examples, task_id = parse_problem(problem)
 
     log_messages = []
@@ -99,7 +99,6 @@ def main():
     wo_example = "_woe" if options.without_example else ""
     dataset_path = f"../../dataset/humaneval{wo_example}.jsonl"
     n_programs = options.number
-    n_shot = options.n_shot
     
     specfix_accuracy_evaluator = SpecFixAccuracyEvaluator(
         differential_tester=differential_tester,
@@ -161,7 +160,7 @@ def main():
 
         results = executor.map(
             lambda args: process_problem(
-                *args, inputs, outputs, specfix_accuracy_evaluator, n_programs, model_name, n_shot
+                *args, inputs, outputs, specfix_accuracy_evaluator, n_programs, model_name
             ),
             tasks
         )
